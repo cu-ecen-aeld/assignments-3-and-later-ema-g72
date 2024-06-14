a8e573a7e30ef92c3df474c4742a0c65742d593c
ema-g72 <emanuele.assenza@gmail.com>
2024-06-14 09:55:39 +0200

    assignment2

---
+++ finder-app/writer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <errno.h>
+#include <syslog.h>
+
+int main(int argc, char *argv[])
+{
+   FILE *fp;
+
+   openlog(NULL, 0, LOG_USER);
+
+   if( argc != 3 )
+   {
+      syslog(LOG_ERR, "Usage: %s <filename> <string>\n", argv[0]);
+      return 1;
+   }
+
+
+   if( (fp = fopen(argv[1], "w")) == NULL )
+   {
+      syslog(LOG_ERR, "Error opening file, errno %d\n", errno);
+      return 1;
+   }
+
+   if( fputs(argv[2], fp) < 0)
+   {
+      syslog(LOG_ERR, "Error opening file, errno %d\n", errno);
+      return -1;
+   }
+
+   return 0;
+}
